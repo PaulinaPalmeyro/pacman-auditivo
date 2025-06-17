3aa8f8894ed355b0aaca60441fd6f6c2d3c733cb
Paulina Palmeyro <paulina.palmeyro@gmail.com>
2025-06-17 00:42:04 -0300

    Vercel

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/api';
+const API_URL = import.meta.env.VITE_API_URL;
+
 
 // Configurar axios para incluir el token en todas las peticiones
 axios.interceptors.request.use((config) => {
